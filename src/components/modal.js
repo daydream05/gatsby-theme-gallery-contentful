0e4af92c13b99497d85d0288f8a0fc344ad32717
Vince P <ianparulan@gmail.com>
2019-07-10 19:19:58 -0400

    added mousetrap

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react'
-import { navigate } from 'gatsby'
+import React, { useState, useEffect } from 'react'
+import { navigate, useStaticQuery, graphql } from 'gatsby'
 import { DialogOverlay, DialogContent } from "@reach/dialog"
 import { css } from 'theme-ui'
+import findIndex from "lodash/findIndex"
+import mousetrap from "mousetrap"
 
 import theme from '../gatsby-plugin-theme-ui'
 
@@ -11,6 +13,65 @@ const Modal = (props) => {
   const { isOpen, location, children } = props
 
   const [showDialog, setShowDialog] = useState(isOpen)
+  const data = useStaticQuery(photoQuery)
+  const photos = data.allContentfulPhoto.edges.map(e => e.node)
+  
+  useEffect(() => {
+    mousetrap.bind(`left`, next)
+    mousetrap.bind(`right`, previous)
+    mousetrap.bind(`space`, next)
+
+    return () => {
+      mousetrap.unbind(`left`)
+      mousetrap.unbind(`right`)
+      mousetrap.unbind(`space`)
+    }
+  }, [`left`, `right`, `space`])
+
+  const findCurrentIndex = () => {
+    let index
+    index = findIndex(
+      photos,
+      photo => photo.fields.path === location.pathname
+    )
+
+    return index
+  }
+
+  const next = (e) => {
+    if(e) {
+      e.stopPropagation()
+    }
+    const currentIndex = findCurrentIndex()
+    if( currentIndex || currentIndex === 0) {
+      let nextPhoto
+      // wrap around if at end
+      if (currentIndex + 1 === photos.length) {
+        nextPhoto = photos[0]
+      } else {
+        nextPhoto = photos[currentIndex + 1]
+      }
+      navigate(`${nextPhoto.fields.path}`);
+    }
+  }
+
+  const previous = (e) => {
+    if(e) {
+      e.stopPropagation()
+    }
+
+    const currentIndex = findCurrentIndex()
+    if (currentIndex || currentIndex === 0) {
+      let previousPhoto
+      // wrap around if at start
+      if (currentIndex === 0) {
+        previousPhoto = photos.slice(-1)[0];
+      } else {
+        previousPhoto = photos[currentIndex - 1]
+      }
+      navigate(`${previousPhoto.fields.path}`);
+    }
+  }
 
   return (
     <DialogOverlay
@@ -39,5 +100,20 @@ const Modal = (props) => {
     </DialogOverlay>
   );
 }
+
+const photoQuery = graphql`
+  query {
+    allContentfulPhoto {
+      edges {
+        node {
+          title
+          fields {
+            path
+          }
+        }
+      }
+    }
+  }
+`
 
 export default Modal
