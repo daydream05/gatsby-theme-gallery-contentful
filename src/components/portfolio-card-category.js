8e5a554bce3debd1892965f67ce719d89ed8d91d
Vince P <ianparulan@gmail.com>
2019-07-12 14:32:31 -0400

    yak

--- src/components/portfolio-card-category.js
+++ src/components/portfolio-card-category.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { css, Styled } from "theme-ui";
+import { css } from "theme-ui";
 
 
 const PortfolioCardCategory = (props) => {
