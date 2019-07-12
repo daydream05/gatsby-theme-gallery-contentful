2885bf1031ede61ac99d9ec86c3737211d3b98d0
Vince P <ianparulan@gmail.com>
2019-07-12 16:40:03 -0400

    fixed

--- src/components/portfolio-page.js
+++ src/components/portfolio-page.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import Layout from './layout'
-import PhotoPageContent from './photo-page-content'
+import PhotoPageContent from './portfolio-page-content'
 
 
 const PortfolioPage = ({ data, location }) => {
