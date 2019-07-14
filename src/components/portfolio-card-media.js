e6f1cadd878dd823e9c25d504c2916fe02daf503
Vince P <ianparulan@gmail.com>
2019-07-14 14:23:22 -0400

    moved video component on its own

--- src/components/portfolio-card-media.js
+++ src/components/portfolio-card-media.js
@@ -1,62 +1,22 @@
-import React, { useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import Img from 'gatsby-image'
-import ReactPlayer from 'react-player'
 import { css } from 'theme-ui'
-import { Waypoint } from 'react-waypoint'
 
-import theme from '../gatsby-plugin-theme-ui'
+import PortfolioCardVideo from './portfolio-card-video'
 
 
 const PortfolioCardMedia = (props) => {
-  const { media, isBehindAModal } = props;
-
-  const [playVideo, setPlayVideo] = useState(false)
+  const { media, isBehindAModal } = props
 
   if (media.file.contentType === "video/mp4") {
     return (
-      <Waypoint
-        onEnter={() => setPlayVideo(true)}
-        onLeave={() => setPlayVideo(false)}
-      >
-        <div
-          css={css({
-            display: `inline-block`,
-          })}
-        >
-          <div
-            css={css({
-              display: "none",
-              overflow: `hidden`,
-              [theme.mediaQueries.lg]: {
-                display: `block`
-              }
-            })}
-          >
-            <ReactPlayer
-              url={media.file.url}
-              muted
-              loop
-              playing={isBehindAModal ? false : playVideo}
-              width="100%"
-              height="auto"
-            />
-          </div>
-          <div
-            css={css({
-              display: `block`,
-              [theme.mediaQueries.lg]: {
-                display: `none`
-              }
-            })}
-          >
-            <ReactPlayer url={media.file.url} width="100%" height="auto" />
-          </div>
-        </div>
-      </Waypoint>
-    );
+      <PortfolioCardVideo
+        isBehindAModal={isBehindAModal}
+        media={media}
+      />
+    )
   }
-  
   return (
     <div>
       {media && (
