a9b6f120b7029822c4b80f12509afc0071e021b5
Vince P <ianparulan@gmail.com>
2019-07-15 19:42:38 -0400

    animated

--- src/components/portfolio-page-share-menu.js
+++ src/components/portfolio-page-share-menu.js
@@ -33,83 +33,90 @@ const PortfolioPageShareMenu = (props) => {
         justifySelf: `end`,
         display: `flex`,
         flexDirection: [`row`, `row`, `row`, `row-reverse`],
+        position: `relative`,
+        overflow: `hidden`
       })}
     >
       <button
         onClick={() => setOpen(!open)}
         css={css({
           background: `unset`,
+          backgroundColor: `white`,
           padding: 0,
           cursor: `pointer`,
+          zIndex: 1,
           ...linkStyle
         })}
       >
         <MdShare />
       </button>
-      {open && (
-        <div
+      <div
+        css={css({
+          display: `flex`,
+          flexDirection: [`row`, `row`, `row`, `row-reverse`],
+          transition: `0.3s transform`,
+          transform: open ? `translateX(0)` : `translateX(100%)`,
+        })}
+      >
+        <a
+          tabIndex={open ? 0 : -1}
+          {...linkAttrs}
+          href={`https://pinterest.com/pin/create/button/${objectToParams({
+            url: url,
+            media: media,
+            description: title
+          })}`}
+          title="Share on Pinterest"
           css={css({
-            display: `flex`,
-            flexDirection: [`row`, `row`, `row`, `row-reverse`]
+            ...linkStyle,
+            borderLeft: `none`,
+            [theme.mediaQueries.md]: {
+              borderLeft: border,
+              borderRight: `none`
+            }
           })}
         >
-          <a
-            {...linkAttrs}
-            href={`https://pinterest.com/pin/create/button/${objectToParams({
-              url: url,
-              media: media,
-              description: title
-            })}`}
-            title="Share on Pinterest"
-            css={css({
-              ...linkStyle,
-              borderLeft: `none`,
-              [theme.mediaQueries.md]: {
-                borderLeft: border,
-                borderRight: `none`
-              }
-            })}
-          >
-            <FaPinterest />
-          </a>
-          <a
-            {...linkAttrs}
-            href={`https://www.facebook.com/sharer.php${objectToParams({
-              u: url,
-              t: title
-            })}`}
-            title="Share on Facebook"
-            css={css({
-              ...linkStyle,
-              borderLeft: `none`,
-              [theme.mediaQueries.md]: {
-                borderLeft: border,
-                borderRight: `none`
-              }
-            })}
-          >
-            <FaFacebookF />
-          </a>
-          <a
-            {...linkAttrs}
-            href={`https://twitter.com/share${objectToParams({
-              url: url,
-              text: title
-            })}`}
-            title="Share on Twitter"
-            css={css({
-              ...linkStyle,
-              borderLeft: `none`,
-              [theme.mediaQueries.md]: {
-                borderLeft: border,
-                borderRight: `none`
-              }
-            })}
-          >
-            <FaTwitter />
-          </a>
-        </div>
-      )}
+          <FaPinterest />
+        </a>
+        <a
+          tabIndex={open ? 0 : -1}
+          {...linkAttrs}
+          href={`https://www.facebook.com/sharer.php${objectToParams({
+            u: url,
+            t: title
+          })}`}
+          title="Share on Facebook"
+          css={css({
+            ...linkStyle,
+            borderLeft: `none`,
+            [theme.mediaQueries.md]: {
+              borderLeft: border,
+              borderRight: `none`
+            }
+          })}
+        >
+          <FaFacebookF />
+        </a>
+        <a
+          tabIndex={open ? 0 : -1}
+          {...linkAttrs}
+          href={`https://twitter.com/share${objectToParams({
+            url: url,
+            text: title
+          })}`}
+          title="Share on Twitter"
+          css={css({
+            ...linkStyle,
+            borderLeft: `none`,
+            [theme.mediaQueries.md]: {
+              borderLeft: border,
+              borderRight: `none`
+            }
+          })}
+        >
+          <FaTwitter />
+        </a>
+      </div>
     </div>
   );
 }
@@ -119,9 +126,12 @@ export default PortfolioPageShareMenu
 
 const border = `2px solid rgba(0,0,0,.2)`;
 
+const openStyle = {
+
+}
 const linkStyle = {
   color: `black`,
-  opacity: 0.5,
+  opacity: 1,
   fontSize: 3,
   width: `48px`,
   height: `48px`,
