b43fd5444621a9cadb28a744debfa8ccfe144700
Vince P <ianparulan@gmail.com>
2019-07-15 16:21:10 -0400

    added share menu state

--- src/components/portfolio-page-share-menu.js
+++ src/components/portfolio-page-share-menu.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaPinterest, FaFacebookF, FaTwitter } from 'react-icons/fa'
 import { MdShare } from 'react-icons/md'
 import { css } from 'theme-ui'
@@ -20,60 +20,82 @@ const objectToParams = object =>
 
 const PortfolioPageShareMenu = (props) => {
   const { url, media, title } = props
+
+  const [open, setOpen] = useState(false)
+
   return (
     <div
       css={css({
         gridColumn: 2,
         gridRow: 1,
         justifySelf: `end`,
+        display: `flex`,
+        flexDirection: `row-reverse`
       })}
     >
-      <div
+      <button
+        onClick={() => setOpen(!open)}
         css={css({
-          display: `flex`,
+          background: `unset`,
+          padding: 0,
+          cursor: `pointer`,
+          ...linkStyle
         })}
       >
-        <a
-          {...linkAttrs}
-          href={`https://pinterest.com/pin/create/button/${objectToParams({
-            url: url,
-            media: media,
-            description: title
-          })}`}
-          title="Share on Pinterest"
+        <MdShare />
+      </button>
+      {open && (
+        <div
           css={css({
-            ...linkStyle,
-            borderRight: `none`
+            display: `flex`,
+            flexDirection: `row-reverse`
           })}
         >
-          <FaPinterest />
-        </a>
-        <a
-          {...linkAttrs}
-          href={`https://www.facebook.com/sharer.php${objectToParams({
-            u: url,
-            t: title
-          })}`}
-          title="Share on Facebook"
-          css={css({
-            ...linkStyle,
-            borderRight: `none`
-          })}
-        >
-          <FaFacebookF />
-        </a>
-        <a
-          {...linkAttrs}
-          href={`https://twitter.com/share${objectToParams({
-            url: url,
-            text: title
-          })}`}
-          title="Share on Twitter"
-          css={css(linkStyle)}
-        >
-          <FaTwitter />
-        </a>
-      </div>
+          <a
+            {...linkAttrs}
+            href={`https://pinterest.com/pin/create/button/${objectToParams({
+              url: url,
+              media: media,
+              description: title
+            })}`}
+            title="Share on Pinterest"
+            css={css({
+              ...linkStyle,
+              borderRight: `none`
+            })}
+          >
+            <FaPinterest />
+          </a>
+          <a
+            {...linkAttrs}
+            href={`https://www.facebook.com/sharer.php${objectToParams({
+              u: url,
+              t: title
+            })}`}
+            title="Share on Facebook"
+            css={css({
+              ...linkStyle,
+              borderRight: `none`
+            })}
+          >
+            <FaFacebookF />
+          </a>
+          <a
+            {...linkAttrs}
+            href={`https://twitter.com/share${objectToParams({
+              url: url,
+              text: title
+            })}`}
+            title="Share on Twitter"
+            css={css({
+              ...linkStyle,
+              borderRight: `none`
+            })}
+          >
+            <FaTwitter />
+          </a>
+        </div>
+      )}
     </div>
   );
 }
@@ -89,7 +111,8 @@ const linkStyle = {
   height: `48px`,
   display: `flex`,
   alignItems: `center`,
-  justifyContent: `center`
+  justifyContent: `center`,
+  boxSizing: `border-box`,
 }
 
 const linkAttrs = {
