419f0502cf126873fd01c348d551e0f5acab4e67
Vince P <ianparulan@gmail.com>
2019-07-15 02:41:38 -0400

    fixed that shttt

--- src/components/gallery-page-masonry.js
+++ src/components/gallery-page-masonry.js
@@ -9,8 +9,8 @@ import theme from '../gatsby-plugin-theme-ui'
 
 const masonryOptions = {
   itemSelector: ".grid-item",
+  columnWidth: `.grid-sizer`,
   gutter: 16,
-  fitWidth: true,
 };
 
 const GalleryPageMasonry = ({ data, isBehindAModal, ...rest }) => {
@@ -18,36 +18,49 @@ const GalleryPageMasonry = ({ data, isBehindAModal, ...rest }) => {
     <ReactMasonry
       options={masonryOptions}
       css={css({
-        mx: `auto`,
-        maxWidth: theme.breakpoints.xl,
         my: 0,
-        [theme.mediaQueries.xl]: {
-          maxWidth: `unset`,
+        width: `100%`,
+        ".grid-sizer": {
+          width: [
+            `calc(99.99% * 1/1 - ${theme.space[3]}px)`,
+            `calc(99.99% * 1/1 - ${theme.space[3]}px)`,
+            `calc(99.99% * 1/1 - ${theme.space[3]}px)`,
+            `calc(99.99% * 1/3 - ${theme.space[3]}px)`,
+            `calc(99.99% * 1/3 - ${theme.space[3]}px)`,
+            `calc(99.99% * 1/3 - ${theme.space[3]}px)`,
+            `calc(99.99% * 1/5 - ${theme.space[3]}px)`
+          ]
         },
         ".grid-item": {
           marginBottom: [3, 4, 3],
+          mx: [2, 2, 2],
           width: [
-            `calc(100vw - ${theme.space[4]}px)`,
-            `calc(100vw - ${theme.space[4]}px)`,
-            `45vw`,
-            `30vw`,
-            `20vw`
+            `calc(99.99% * 1/1 - ${theme.space[3]}px)`,
+            `calc(99.99% * 1/1 - ${theme.space[3]}px)`,
+            `calc(99.99% * 1/1 - ${theme.space[3]}px)`,
+            `calc(99.99% * 1/3 - ${theme.space[3]}px)`,
+            `calc(99.99% * 1/3 - ${theme.space[3]}px)`,
+            `calc(99.99% * 1/3 - ${theme.space[3]}px)`,
+            `calc(99.99% * 1/5 - ${theme.space[3]}px)`
           ]
-        },
+        }
       })}
       {...rest}
     >
+      <div className="grid-sizer" />
       {data.allContentfulPortfolio.edges.map(({ node }, index) => {
         return (
           <PortfolioCard
             isBehindAModal={isBehindAModal}
             key={`${node.id}-${index}`}
-            className="grid-item"
+            className={`grid-item`}
             title={node.title}
             media={node.media}
             category={node.category}
             linkTo={node.fields.path}
-            description={node.description && node.description.internal.content}
+            description={
+              node.description && node.description.internal.content
+            }
           />
         );
       })}
